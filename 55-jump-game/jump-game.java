7d4e37fd574db3b2c70bd894f4d581331225135a
hivespace88 <hivespace88@gmail.com>
2026-01-23 12:00:20 +0530

    Time: 2 ms (85.09%) | Memory: 47.7 MB (85.24%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        int result = n-1;
 
-        for(int i = n-2; i>=0; i--){
-            if(i+nums[i] >= result){
-                result = i;
+        int farthest =0;
+
+        for(int i=0; i<n;i++){
+
+            if(i>farthest){
+                return false;
             }
+
+            farthest = Math.max(farthest, i+nums[i]);
         }
-        if(result == 0){
-            return true;
-        }
-        return false;
+        return true;
     }
 }
