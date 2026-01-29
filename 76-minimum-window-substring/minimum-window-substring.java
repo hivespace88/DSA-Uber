ef5c57a9011720b4f0ee30c45cc8250ebcc1f61e
hivespace88 <hivespace88@gmail.com>
2026-01-29 19:01:07 +0530

    Time: 19 ms (46.63%) | Memory: 47.7 MB (37.72%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,40 +1,48 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int[] arr = new int[128];
-        char[] s_arr = s.toCharArray();
-        char[] t_arr = t.toCharArray();
 
-        for(char c : t_arr){
-            arr[c]++;
-        }
+       Map<Character, Integer> need = new HashMap();
 
-        int l=0,r =0;
-        int minlen =Integer.MAX_VALUE;
-        int counter =0;
-        String ans ="";
+       for(char ch : t.toCharArray()){
+        need.put(ch, need.getOrDefault(ch,0)+1);
+       }
 
-        while(r < s_arr.length){
-            char currchar= s_arr[r];
-            if(--arr[currchar] >= 0){
-                counter++;
+       int required = need.size();
+       int formed = 0; 
+       int minLen = Integer.MAX_VALUE;
+       int start = 0;
+
+       int left =0;
+       int right =0;
+
+       Map<Character,Integer> window = new HashMap();
+
+       while(right < s.length()){
+         
+         char c = s.charAt(right);
+         window.put(c, window.getOrDefault(c,0)+1);
+         if(need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()){
+            formed++;
+         }
+
+         
+
+         while(left <= right && formed == required){
+
+            if(right-left+1 < minLen){
+                minLen = right-left+1;
+                start = left;
             }
+           char removechar = s.charAt(left);
+           window.put(removechar, window.get(removechar)-1);
 
-            
-            while(counter == t_arr.length){
-              int windowsize = r-l+1;
-              if(windowsize < minlen){
-                 minlen =windowsize;
-                 ans = s.substring(l, r+1);
-              }
-              int leftchar = s_arr[l];
-              if(++arr[leftchar]>0){
-                counter--;
-              }
-              l++;
-            }
-            r++;
-        }
-
-        return ans;
+           if(need.containsKey(removechar) && window.get(removechar) < need.get(removechar)){
+            formed--;
+           }
+           left++;
+         }
+         right++;
+       }
+       return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start+minLen);
     }
 }
