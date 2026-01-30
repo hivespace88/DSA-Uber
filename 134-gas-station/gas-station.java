6e880d50465e83d76be2760d183f8359e2ee98f9
hivespace88 <hivespace88@gmail.com>
2026-01-30 16:02:19 +0530

    Time: 2 ms (99.02%) | Memory: 119.8 MB (6.60%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,24 +1,25 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalgas =0;
+        
         int totalcost =0;
+        int totalgas =0;
 
-        for(int i =0 ; i<gas.length; i++){
+        for(int i=0;i<gas.length;i++){
             totalgas += gas[i];
             totalcost += cost[i];
         }
 
-        if(totalgas<totalcost) return -1;
+        if(totalgas < totalcost) return -1;
 
         int tank = 0;
         int start =0;
 
-        for(int i=0;i<gas.length;i++){
+        for(int i=0; i<gas.length; i++){
             tank += gas[i]-cost[i];
 
-            if(tank <0 ){
-                start = i+1;
+            if(tank < 0){
                 tank =0;
+                start = i+1;
             }
         }
        return start;
