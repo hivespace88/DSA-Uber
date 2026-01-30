392538aa88a635a41d0f3013e88135efcdab2f23
hivespace88 <hivespace88@gmail.com>
2026-01-30 12:18:37 +0530

    Time: 2 ms (35.47%) | Memory: 47.5 MB (15.12%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,17 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
         int farthest = 0;
-        int jumps =0;
         int currentEnd =0;
+        int jumps = 0;
 
-        for(int i=0;i<nums.length-1;i++){
-            farthest = Math.max(farthest, i+nums[i]);
+        for(int i =0 ; i<nums.length-1; i++){
+            farthest = Math.max(farthest , i+nums[i]);
 
             if(i == currentEnd){
-                jumps++;
                 currentEnd = farthest;
+                jumps++;
             }
         }
+
         return jumps;
     }
 }
