f2b2c6e6b9b9c330e72e01b90777986f5cec9179
hivespace88 <hivespace88@gmail.com>
2026-02-02 17:06:22 +0530

    Time: 19 ms (46.04%) | Memory: 47.9 MB (21.09%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,48 +1,48 @@
 class Solution {
     public String minWindow(String s, String t) {
-
+       
        Map<Character, Integer> need = new HashMap();
-
+       
        for(char c : t.toCharArray()){
-        need.put(c, need.getOrDefault(c,0)+1);
+          need.put(c, need.getOrDefault(c,0)+1);
        }
 
        int required = need.size();
-
-       int left = 0;
-       int right = 0 ;
-       int formed = 0;
-
-       int minLen = Integer.MAX_VALUE;
+       int left =0, right=0;
+       int formed =0;
+       int minLen =Integer.MAX_VALUE;
        int start = 0;
-   
-    Map<Character, Integer> window = new HashMap();
-       while(right < s.length()){
-             char ch =  s.charAt(right);
-             window.put(ch, window.getOrDefault(ch,0)+1);
 
-             if(need.containsKey(ch) && window.get(ch).intValue() == need.get(ch).intValue()){
-                formed++;
-             }   
-             
-             while(left <= right && required == formed){
+       Map<Character, Integer> window = new HashMap(); 
 
-                  if(right-left+1 < minLen){
-                    minLen = right-left+1;
-                    start = left;
-                  }
+        while (right < s.length()){
+            
+            char c = s.charAt(right);
+            
+            window.put(c, window.getOrDefault(c,0)+1);
 
-                  char removechar = s.charAt(left);
-                  window.put(removechar, window.get(removechar)-1);
+                if(need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()){
+                    formed++;
+                }
 
-                  if(need.containsKey(removechar) && window.get(removechar) < need.get(removechar)){
-                    formed--;
-                  }
-                  left++;
-             }
-             right++;
-       }
+                while(left <= right && required == formed ){
 
+                    if(right-left+1 < minLen){
+                        minLen = right-left+1;
+                        start = left;
+                    }
+
+                    char removechar = s.charAt(left);
+                    window.put(removechar, window.get(removechar)-1);
+
+                        if(need.containsKey(removechar) &&
+                            window.get(removechar) < need.get(removechar)){
+                                formed--;
+                            }
+                    left++;        
+                }
+            right++;    
+        }
        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start+minLen);
     }
 }
