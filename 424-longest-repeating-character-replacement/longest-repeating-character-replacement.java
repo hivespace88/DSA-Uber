fe2361045884494cd9984bb875a75998edc3a750
hivespace88 <hivespace88@gmail.com>
2026-02-19 18:15:49 +0530

    Time: 12 ms (48.19%) | Memory: 46.3 MB (51.00%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,23 +1,28 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-     int[] freq = new int[26];
-     int left = 0;
-     int maxfreq = 0;
-     int maxLen = 0;
+       
+       int[]  freq = new int[26];
+       int left = 0;
+       int maxfreq = 0;
+       int maxwindow = 0;
 
-     for(int right = 0; right < s.length(); right++){
-        freq[s.charAt(right)-'A']++;
+       for(int right =0; right<s.length();right++){
+           
+           freq[s.charAt(right) -'A']++;
+           maxfreq = Math.max(maxfreq , freq[s.charAt(right)-'A']);
 
-        maxfreq = Math.max(maxfreq, freq[s.charAt(right)-'A']);
+           int windowsize = right-left+1;
 
-        if((right-left+1)-maxfreq >k){
-            freq[s.charAt(left)-'A']--;
-            left++;
-        }
+           if(windowsize -maxfreq > k){
+             freq[s.charAt(left) -'A']--;
+             left++;
+           }
 
-        maxLen = Math.max(maxLen, right-left+1);
-     }
-     return maxLen;
+           windowsize = right-left+1;
+
+           maxwindow = Math.max(maxwindow, windowsize);
+       }
+    
+     return maxwindow;
     }
-
 }
