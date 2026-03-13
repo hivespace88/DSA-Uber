1aed4146f26cbbcd85218e626d0398c55553212c
hivespace88 <hivespace88@gmail.com>
2026-03-13 14:21:17 +0530

    Time: 11 ms (95.58%) | Memory: 47.6 MB (63.59%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,35 +1,35 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> countMap = new HashMap();
+        Map<Integer,Integer> countMap = new HashMap();
 
         for(int n : nums){
             countMap.put(n, countMap.getOrDefault(n,0)+1);
-        }        
+        }
 
         List<Integer>[] bucket = new List[nums.length+1];
 
         for(int key : countMap.keySet()){
-                 int fequency =    countMap.get(key);
-            if(bucket[fequency] == null){
-                bucket[fequency] = new ArrayList();
+            int frequency = countMap.get(key);
+            
+            if(bucket[frequency] == null){
+                bucket[frequency] = new ArrayList();
             }
-
-            bucket[fequency].add(key);
+            bucket[frequency].add(key);
         }
 
-        int[] result = new int[k];
-        int index = 0;
+         int[] result = new int[k];
+         int index =0;
 
-        for(int i = bucket.length-1; i>=0 && index <k ; i--){
+         for(int  i = bucket.length-1; i>=0 && index <k ; i--){
+
             if(bucket[i] != null){
-                for(int n : bucket[i]){
-                    result[index++] = n;
-                    if(index == k ){
-                        return result;
-                    }
+                for(int num : bucket[i]){
+                    result[index++] = num;
+                    if(index == k) return result;
                 }
             }
-        }
+         } 
+
         return result;
     }
 }
